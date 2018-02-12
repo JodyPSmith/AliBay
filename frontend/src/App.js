516fb997da00a1722cb722aa2040e43aac704843
cev <seeve.c@gmail.com>
2018-02-12 12:04:03 -0500

    added tacyhons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './images/Alibay.png';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <img alt="logo" src={logo} />
+            </div>
+        );
+    }
 }
 
 export default App;
