13282208f8ef8f58cd2ab57f7bfe17b14ecb3804
cev <seeve.c@gmail.com>
2018-02-12 12:57:57 -0500

    added search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import logo from './images/Alibay.png';
 import LogIn from './components/google-OAuth/googleOauth';
+import SearchBar from './components/searchBar/searchBar';
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <img alt="logo" src={logo} />
+                <SearchBar />
                 {/*<LogIn />*/}
             </div>
         );
