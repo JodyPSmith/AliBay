8ec3352aae61351fe191d6d9b4bbcbf6978a151e
cev <seeve.c@gmail.com>
2018-02-12 13:11:57 -0500

    added some css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './images/Alibay.png';
 import LogIn from './components/google-OAuth/googleOauth';
 import SearchBar from './components/searchBar/searchBar';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <img alt="logo" src={logo} />
+            <div className="App ">
+                {/*<Navigation />*/}
                 <SearchBar />
                 {/*<LogIn />*/}
             </div>
--- frontend/src/components/searchBar/searchBar.js
+++ frontend/src/components/searchBar/searchBar.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import logo from '../../images/Alibay.png';
 
 class SearchBar extends Component {
     render() {
         return (
-            <div className="">
-                <input type="text" name="search" />
-                <input type="submit" value="Go" />
+            <div className="flex flex-column justify-center content-center ">
+                <div className="">
+                    <img alt="logo" src={logo} />
+                </div>
+                <div>
+                    <input type="text" name="search" />
+                    <input type="submit" value="Go" />
+                </div>
             </div>
         );
     }
