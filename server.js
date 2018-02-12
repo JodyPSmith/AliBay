2466cff0551b578818239a4d8fdb5661701a8719
vbakarian <vako.bakarian@gmail.com>
2018-02-12 12:34:14 -0500

    serverjs and alibayjs created

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+const alibay = require('./alibay')
 var express = require('express');
 var app = express();
 var fs = require('fs');
 
 app.get('/', (req, res) => {
-    res.send('hello, server is working')
+    var x = alibay.genUID();
+    res.send(""+ x)
 })
+
+app.get('/itemsBought', (req, res) => {
+    let uid = req.query.uid;
+    res.send(JSON.stringify(alibay.getItemsBought(uid)));
+});
+
 
 app.post('data', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log('server started on http://jodysmith.ca:3000')
+app.listen(4000, () => {
+    console.log('Listening on port 4000')
 })
