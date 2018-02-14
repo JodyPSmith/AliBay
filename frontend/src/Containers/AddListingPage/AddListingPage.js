dd079c82b80767146f37c0e3723d283a8d05942f
Jody Smith <jodypsmith@gmail.com>
2018-02-14 11:22:43 -0500

    modified signup and addlisting

--- frontend/src/Containers/AddListingPage/AddListingPage.js
+++ frontend/src/Containers/AddListingPage/AddListingPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import AddListing from '../../components/addListing/addListing'
-import logo from '../../images/Alibay.png'
+import AddListing from '../../components/addListing/addListing';
+import Navigation from '../../components/Navigation/Navigation'
+import logo from '../../images/Alibay.png';
 
 class AddListingPage extends Component {
     render() {
+        const { signedIn, route, setRoute } = this.props;
         return (
             <div>
+                <Navigation 
+                setRoute={setRoute}
+                route={route}
+                signedIn={signedIn}/>
                 <img alt="Alibay Logo" src={logo}/>
                 <AddListing setRoute={this.props.setRoute}/>
             </div>
