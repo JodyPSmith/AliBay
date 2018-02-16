ff453634d14b15d4021c381af7e754a5d7f0787e
jytsong <jyt.song@gmail.com>
2018-02-15 19:15:21 -0500

    finished createListing endpoint image association

--- backend/alibay.js
+++ backend/alibay.js
@@ -1,14 +1,18 @@
 const assert = require("assert");
 const fs = require("fs");
 const bcrypt = require("bcrypt");
-const mysql = require('promise-mysql');
-var con = null
-mysql.createConnection({
-  host: '165.227.39.184',
-  user: 'vako',
-  password: '12345',
-  database: 'alibay'
-}).then((x) => { con = x });
+const mysql = require("promise-mysql");
+var con = null;
+mysql
+  .createConnection({
+    host: "165.227.39.184",
+    user: "vako",
+    password: "12345",
+    database: "alibay"
+  })
+  .then(x => {
+    con = x;
+  });
 
 // Maps:
 // map that keeps track of all the items a user has bought
@@ -32,24 +36,23 @@ let userMap = {
   }
 };
 
-//map that keeps track of passwords
-// let passMap = {};
+//map that keeps track of images by listing
+let imgMap = {};
 
 // map that keeps track of all products
 let productsMap = {};
 
 // initialize maps from data file
 try {
-  console.log('Reading files...')
-  itemsBought = JSON.parse(fs.readFileSync('./datafiles/itemsBought.txt'));
-  itemsSold = JSON.parse(fs.readFileSync('./datafiles/itemsSold.txt'));
-  userMap = JSON.parse(fs.readFileSync('./datafiles/userMap.txt'));
+  console.log("Reading files...");
+  itemsBought = JSON.parse(fs.readFileSync("./datafiles/itemsBought.txt"));
+  itemsSold = JSON.parse(fs.readFileSync("./datafiles/itemsSold.txt"));
+  userMap = JSON.parse(fs.readFileSync("./datafiles/userMap.txt"));
   // passMap = JSON.parse(fs.readFileSync('./datafiles/passMap.txt'));
-  productsMap = JSON.parse(fs.readFileSync('./datafiles/productsMap.txt'));
-}
-catch (err) {
-  console.log('error encountered; data file probably not initialized:')
-  console.log(`${err}`)
+  productsMap = JSON.parse(fs.readFileSync("./datafiles/productsMap.txt"));
+} catch (err) {
+  console.log("error encountered; data file probably not initialized:");
+  console.log(`${err}`);
 }
 
 /*
@@ -93,12 +96,11 @@ async function signUp(
     };
     // itemsBought[userID] = [];
     // itemsSold[userID] = [];
-    con.query('INSERT INTO users SET ?', userInfo,
-      (err, rows) => {
-        if (err) throw err;
-        console.log('Data inserted into Db:\n');
-        console.log(rows);
-      });
+    con.query("INSERT INTO users SET ?", userInfo, (err, rows) => {
+      if (err) throw err;
+      console.log("Data inserted into Db:\n");
+      console.log(rows);
+    });
     // fs.writeFileSync("././datafiles/itemsBought.txt", JSON.stringify(itemsBought));
     // fs.writeFileSync("././datafiles/itemsSold.txt", JSON.stringify(itemsSold));
     // fs.writeFileSync("././datafiles/userMap.txt", JSON.stringify(userMap));
@@ -106,26 +108,31 @@ async function signUp(
     console.log(`user created`);
     return true;
   } else {
-    console.log('check username failed')
+    console.log("check username failed");
     return false;
   }
 }
 
 async function login(username, password) {
-  var result = await con.query('SELECT id, password FROM users WHERE username = ?', [username])
+  var result = await con.query(
+    "SELECT id, password FROM users WHERE username = ?",
+    [username]
+  );
   console.log(">>> LOGIN RESULT >> ", result[0].password);
   if (result[0] !== undefined) {
     var bool = bcrypt.compareSync(password, result[0].password);
-    return { id: result[0].id, result: bool }
-  }
-  else {
-    return false
+    return { id: result[0].id, result: bool };
+  } else {
+    return false;
   }
 }
 
 async function checkUsername(username) {
-  var result = await con.query('SELECT username FROM users WHERE username = ?', [username])
-  console.log(`result :`, result)
+  var result = await con.query(
+    "SELECT username FROM users WHERE username = ?",
+    [username]
+  );
+  console.log(`result :`, result);
   if (!result[0]) {
     return true;
   } else {
@@ -138,7 +145,9 @@ function getUserID(username) {
   let allUserIDs = Object.keys(userMap);
   let ret;
   allUserIDs.forEach(x => {
-    if (username == userMap[x].username) { ret = x; }
+    if (username == userMap[x].username) {
+      ret = x;
+    }
   });
   return ret;
 }
@@ -154,20 +163,22 @@ This function is incomplete. You need to complete it.
       [desc] A desc describing the item
     returns: The ID of the new listing
 */
-function createListing(title, sellerID, price, desc) {
-  let pID = genPID();
-  productsMap[pID] = {
+function createListing(sellerID, title, price, desc, images, location) {
+  let productMap = {
     title: title,
     description: desc,
     price: price,
-    seller_id: sellerID,
+    seller_id: sellerID
   };
-  con.query('INSERT INTO listing SET ?', productsMap[pID], (err, rows) => {
+  if (imgMap[sellerID]) imgMap[sellerID] = imgMap[sellerID].concat(images);
+  else imgMap[sellerID] = images;
+  console.log(`imgMap[${sellerID}]: ${imgMap[sellerID]}`);
+  fs.writeFileSync('./datafiles/imgMap.txt', JSON.stringify(imgMap));
+  con.query("INSERT INTO listing SET ?", productMap, (err, rows) => {
     if (err) throw err;
-    console.log('Data received from Db:\n');
+    console.log("Data received from Db:\n");
     console.log(rows);
   });
-  return pID;
   //fs.writeFileSync("./backend/./datafiles/productsMap.txt", JSON.stringify(productsMap));
 }
 
@@ -184,13 +195,17 @@ The seller will see the listing in his history of items sold
     returns: undefined
 */
 function buy(userID, listingID) {
-  con.query('UPDATE listing SET buyer_id ?' , [userID] , 'WHERE listing_id = ?', [listingID], (err, rows) => {
-    if (err) throw err;
-    console.log('Data received from Db:\n');
-    console.log(rows);
-  });
-
-  
+  con.query(
+    "UPDATE listing SET buyer_id ?",
+    [userID],
+    "WHERE listing_id = ?",
+    [listingID],
+    (err, rows) => {
+      if (err) throw err;
+      console.log("Data received from Db:\n");
+      console.log(rows);
+    }
+  );
 
   //fs.writeFileSync("././datafiles/itemsBought.txt", JSON.stringify(itemsBought));
   //fs.writeFileSync("././datafiles/itemsSold.txt", JSON.stringify(itemsSold));
@@ -205,7 +220,9 @@ allItemsBought returns the IDs of all the items bought by a buyer
     returns: an array of listing IDs
 */
 async function allItemsBought(buyerID) {
-  var result = await con.query('SELECT * FROM listing WHERE buyer_id = ?', [buyerID]);
+  var result = await con.query("SELECT * FROM listing WHERE buyer_id = ?", [
+    buyerID
+  ]);
   console.log(">>> Items bought by user >>>", result);
   return result;
 }
@@ -216,13 +233,19 @@ allItemsSold returns the IDs of all the items sold by a seller
 */
 async function allItemsSold(sellerID) {
   var listing_id = [];
-  var result = await con.query('SELECT * FROM listing WHERE seller_id = ?', [sellerID]);
-  console.log(">>>", result)
+  var result = await con.query("SELECT * FROM listing WHERE seller_id = ?", [
+    sellerID
+  ]);
+  console.log(">>>", result);
   return result;
 }
 // return items that the user is currently selling.
-async function allitemsSelling(sellerID){
-  var result = await con.query('SELECT * FROM listing WHERE seller_id =?' , [sellerID], 'AND buyer_id IS NULL')
+async function allitemsSelling(sellerID) {
+  var result = await con.query(
+    "SELECT * FROM listing WHERE seller_id =?",
+    [sellerID],
+    "AND buyer_id IS NULL"
+  );
   console.log(">>>", result);
   return result;
 }
@@ -234,7 +257,7 @@ Once an item is sold, it will not be returned by allListings
 async function allListings() {
   // let productIds = Object.keys(productsMap); // array of all productIds in map
   // return productIds.filter(x => !productsMap[x].isSold); // return new array with all products where isSold == true
-  var result = await con.query('SELECT * FROM listing WHERE buyer_id IS NULL');
+  var result = await con.query("SELECT * FROM listing WHERE buyer_id IS NULL");
   return result;
 }
 
@@ -275,7 +298,7 @@ module.exports = {
   itemsSold,
   productsMap,
   userMap,
-  // passMap,
+  imgMap,
   genUID,
   genPID,
   signUp,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const alibay = require('./alibay');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const express = require('express');
+const alibay = require("./alibay");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const busboy = require('connect-busboy');
+const cookieParser = require("cookie-parser");
+const busboy = require("connect-busboy");
 
 app.use(cookieParser());
 
@@ -14,176 +14,176 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(busboy());
 
-app.use(express.static('datafiles/tempImages'));
+app.use(express.static("datafiles/tempImages"));
 
 var cookieMap = {};
 
 try {
-    cookieMap = JSON.parse(fs.readFileSync('./datafiles/cookieMap.txt'));
+  cookieMap = JSON.parse(fs.readFileSync("./datafiles/cookieMap.txt"));
 } catch (err) {
-    console.log('cookieMap.txt not found');
+  console.log("cookieMap.txt not found");
 }
 
 const generateCookie = () => {
-    let sessionID = 's' + Math.floor(Math.random() * 100000);
-    if (cookieMap[sessionID]) {
-        return generateCookie();
-    } else return sessionID; //if sessionID exists, re-generate sessionID
+  let sessionID = "s" + Math.floor(Math.random() * 100000);
+  if (cookieMap[sessionID]) {
+    return generateCookie();
+  } else return sessionID; //if sessionID exists, re-generate sessionID
 };
 
 //signup / login endpoints----------------------------------------------------------------------------------------
-app.post('/signUp', async (req, res) => {
-    let request = req.body;
-    let fname = request.firstname;
-    let lname = request.lastname;
-    let usr = request.username;
-    let pwd = request.password;
-    let email = request.email;
-    let address = request.address;
-    let city = request.city;
-    let province = request.province;
-    let postal_code = request.postal_code;
-    let country = request.country;
+app.post("/signUp", async (req, res) => {
+  let request = req.body;
+  let fname = request.firstname;
+  let lname = request.lastname;
+  let usr = request.username;
+  let pwd = request.password;
+  let email = request.email;
+  let address = request.address;
+  let city = request.city;
+  let province = request.province;
+  let postal_code = request.postal_code;
+  let country = request.country;
 
-    if (
-        await alibay.signUp(
-            fname,
-            lname,
-            usr,
-            pwd,
-            email,
-            address,
-            city,
-            province,
-            postal_code,
-            country
-        )
-    ) {
-        res.send('signup success');
-    } else {
-        res.send('signup failed');
-    }
+  if (
+    await alibay.signUp(
+      fname,
+      lname,
+      usr,
+      pwd,
+      email,
+      address,
+      city,
+      province,
+      postal_code,
+      country
+    )
+  ) {
+    res.send("signup success");
+  } else {
+    res.send("signup failed");
+  }
 });
-app.post('/login', async (req, res) => {
-    // takes object with username & password, attempts to match with database
-    let payload = req.body;
-    let username = payload.username;
-    let password = payload.password;
-    var test = await alibay.login(username, password);
-    if (test.result) {
-        let sessionID = generateCookie();
-        cookieMap[sessionID] = test.id;
-        fs.writeFileSync(
-            __dirname + '/datafiles/cookieMap.txt',
-            JSON.stringify(cookieMap)
-        );
-        res.set('Set-Cookie', 'sessionID=' + sessionID);
-        res.send({ res: true, sessionID: sessionID }); //if successful, will send JSON object with sessionID
-    } else {
-        res.send({ res: false }); //if failed login, send JSON object with sessionID false
-    }
+app.post("/login", async (req, res) => {
+  // takes object with username & password, attempts to match with database
+  let payload = req.body;
+  let username = payload.username;
+  let password = payload.password;
+  var test = await alibay.login(username, password);
+  if (test.result) {
+    let sessionID = generateCookie();
+    cookieMap[sessionID] = test.id;
+    fs.writeFileSync(
+      __dirname + "/datafiles/cookieMap.txt",
+      JSON.stringify(cookieMap)
+    );
+    res.set("Set-Cookie", "sessionID=" + sessionID);
+    res.send({ res: true, sessionID: sessionID }); //if successful, will send JSON object with sessionID
+  } else {
+    res.send({ res: false }); //if failed login, send JSON object with sessionID false
+  }
 });
 
 //user actions that affect product/inventory-----------------------------------------------------------------------
 
-app.post('/buy', (req, res) => {
-    // takes a JSON object in body, with sellerID, listingID returns object with res:true
-    let sessionID = req.cookies.sessionID;
-    let sellerID = cookieMap[sessionID];
+app.post("/buy", (req, res) => {
+  // takes a JSON object in body, with sellerID, listingID returns object with res:true
+  let sessionID = req.cookies.sessionID;
+  let sellerID = cookieMap[sessionID];
 
-    let request = req.body;
-    let buyerID = request.buyerID;
-    let listingID = request.listingID;
-    alibay.buy(buyerID, sellerID, listingID);
-    res.send(JSON.stringify({ res: true }));
+  let request = req.body;
+  let buyerID = request.buyerID;
+  let listingID = request.listingID;
+  alibay.buy(buyerID, sellerID, listingID);
+  res.send(JSON.stringify({ res: true }));
 });
 
-app.post('/createListing', (req, res) => {
-    // takes a JSON object in body, with title, image(s), price, desc, and returns productID string
-    let sessionID = req.cookies.sessionID;
-    let sellerID = cookieMap[sessionID];
+app.post("/createListing", (req, res) => {
+  // takes a JSON object in body, with title, image(s), price, desc, and returns productID string
+  let sessionID = req.cookies.sessionID;
+  let sellerID = cookieMap[sessionID];
 
-    let request = req.body;
+  let request = req.body;
 
-    var image1 = request.images[0].preview;
-    let title = request.title;
-    let price = request.price;
-    let desc = request.description;
-    res.send(
-        'your product ID is: ' +
-            alibay.createListing(title, sellerID, price, desc)
-    );
+  let title = request.title;
+  let price = request.price;
+  let desc = request.description;
+  let imageArr = request.images;
+  let loc = request.location;
+  
+  alibay.createListing(sellerID, title, price, desc, imageArr, loc);
+
+  res.send({res: "product created"});
 });
 
 // itemInfo that takes sessionID ---------------------------------------------------------------------
 
-app.get('/itemsBought', (req, res) => {
-    // takes cookie, returns array of all items bought buy the user
-    let sessionID = req.cookies.sessionID;
-    let userID = cookieMap[sessionID];
-    // console.log(`sessionID=${sessionID}, userID=${userID}`)
-    res.send(alibay.getItemsBought(userID));
+app.get("/itemsBought", (req, res) => {
+  // takes cookie, returns array of all items bought buy the user
+  let sessionID = req.cookies.sessionID;
+  let userID = cookieMap[sessionID];
+  // console.log(`sessionID=${sessionID}, userID=${userID}`)
+  res.send(alibay.getItemsBought(userID));
 });
 
-app.post('/itemsSold', async (req, res) => {
-    // takes single string in body, returns arrray of listing IDs
-    var payload = JSON.parse(req.body.toString());
-    console.log('>>>>>>>', payload.seller_id);
-    var ok = await alibay.allItemsSold(payload.seller_id);
-    console.log(ok);
-    res.send(ok);
+app.post("/itemsSold", async (req, res) => {
+  // takes single string in body, returns arrray of listing IDs
+  var payload = JSON.parse(req.body.toString());
+  console.log(">>>>>>>", payload.seller_id);
+  var ok = await alibay.allItemsSold(payload.seller_id);
+  console.log(ok);
+  res.send(ok);
 });
 
-app.get('/itemDescription', (req, res) => {
-    // Returns object with price and blurb
-    let item = req.query.item;
-    let description = alibay.getItemDescription(item);
-    res.send(description);
+app.get("/itemDescription", (req, res) => {
+  // Returns object with price and blurb
+  let item = req.query.item;
+  let description = alibay.getItemDescription(item);
+  res.send(description);
 });
 
 // search functions-----------------------------------------------------------------------------------
 
-app.get('/allListings', (req, res) => {
-    // returns an array with all listings where isSold === true
-    res.send(alibay.allListings());
+app.get("/allListings", (req, res) => {
+  // returns an array with all listings where isSold === true
+  res.send(alibay.allListings());
 });
 
-app.post('/search', (req, res) => {
-    // returns new array where description includes search term ***To be optimized later***
-    let request = req.body.toString();
-    let searchTerm = request.searchTerm;
-    let results = alibay.searchForListings(searchTerm);
-    res.send(results); // return the array (could be empty) to be processed in front-end
+app.post("/search", (req, res) => {
+  // returns new array where description includes search term ***To be optimized later***
+  let request = req.body.toString();
+  let searchTerm = request.searchTerm;
+  let results = alibay.searchForListings(searchTerm);
+  res.send(results); // return the array (could be empty) to be processed in front-end
 });
 
 app.listen(4000, () => {
-    console.log('Listening on port 4000');
+  console.log("Listening on port 4000");
 });
 
 // image filewriting
 
-app.post('/imgUpload', (req, res) => {
-    var fstream;
-    var filenames = [];
-    req.pipe(req.busboy);
-    req.busboy.on('file', (fieldname, file, filename) => {
-        console.log(file);
-        filename = 'img' + Math.floor(Math.random() * 10000000);
-        console.log('Uploading: ' + filename);
-        fstream = fs.createWriteStream(
-            __dirname + '/datafiles/tempImages/' + filename
-        );
-        filenames.push(filename);
-        file.pipe(fstream);
-        fstream.on('error', err => {
-            console.log('error', err);
-            res.status(500).send({ res: 'error' });
-        });
-    });
-    req.busboy.on('finish', () =>
-        res.send({
-            res: 'successful',
-            images: filenames
-        })
+app.post("/imgUpload", (req, res) => {
+  var fstream;
+  var filenames = [];
+  req.pipe(req.busboy);
+  req.busboy.on("file", (fieldname, file, filename) => {
+    filename = "img" + Math.floor(Math.random() * 10000000);
+    console.log("Uploading: " + filename);
+    fstream = fs.createWriteStream(
+      __dirname + "/datafiles/tempImages/" + filename
     );
+    filenames.push(filename);
+    file.pipe(fstream);
+    fstream.on("error", err => {
+      console.log("error", err);
+      res.status(500).send({ res: "error" });
+    });
+  });
+  req.busboy.on("finish", () =>
+    res.send({
+      res: "successful",
+      images: filenames
+    })
+  );
 });
